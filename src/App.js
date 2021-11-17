9444bf2e25169d62c4963c895a6c2570d86e4a94
pnut-work <ppeebb10@naver.com>
2021-11-18 00:05:58 +0900

    link apply

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
-import MainPageComponent from './main/index.js';
-
+import MainPageComponent from './main';
+import UploadPage from './upload';
+import ProductPage from './product';
+import {Switch, Route} from 'react-router-dom';
 function App() {
   return (
-    <MainPageComponent />
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>       
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function MainPage() {
   const [products, setProducts]= React.useState([]);
@@ -30,17 +31,19 @@ function MainPage() {
             products.map(function (product, index) {
               return (
                 <div className="product-card">
-                  <div>
-                    <img className="product-img" src={product.imageUrl} alt="product" />
-                  </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <span className="product-seller">
-                      <img className="product-avatar" src="images/icons/avatar.png" alt="avatar" />
-                      <span>{product.seller}</span>
-                    </span>
-                  </div>
+                  <Link className="product-link" to={`/products/${index}`}>
+                    <div>
+                      <img className="product-img" src={product.imageUrl} alt="product" />
+                    </div>
+                    <div className="product-contents">
+                      <span className="product-name">{product.name}</span>
+                      <span className="product-price">{product.price}원</span>
+                      <span className="product-seller">
+                        <img className="product-avatar" src="images/icons/avatar.png" alt="avatar" />
+                        <span>{product.seller}</span>
+                      </span>
+                    </div>
+                  </Link>
                 </div>
               )
             })
